fe60a11389bdf0ec49b1980b97051de0854a2c87
a-sakhizada <arezousakhizada@gmail.com>
2022-07-27 16:59:37 -0400

    installed ejs and set the view engine to EJS

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
+//set ejs as the view engine 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "https://www.google.com"
@@ -11,10 +14,12 @@ app.get("/", (req, res) => {
     res.send("Hello!");
 });
 
+
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
+//sending HTML (rendered on client)
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
