c374a62696410cfe4298fecf8cce5d1b9eccbe61
a-sakhizada <arezousakhizada@gmail.com>
2022-07-27 17:32:12 -0400

    sending urldatabase data to urls_index.ejs through templateVars

--- express_server.js
+++ express_server.js
@@ -2,28 +2,33 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 
-//set ejs as the view engine 
+//set ejs as the view engine
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "https://www.google.com"
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "https://www.google.com",
 };
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+  res.send("Hello!");
 });
 
-
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+  res.json(urlDatabase);
 });
 
 //sending HTML (rendered on client)
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+//send urlDatabase data through tempalteVars to urls_index.ejs
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port ${PORT}`);
 });
