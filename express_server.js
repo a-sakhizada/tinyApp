51be2775fddbf6b989e405063704ada2e6a99a91
a-sakhizada <arezousakhizada@gmail.com>
2022-07-29 02:03:22 -0400

    modified post /register to handle error conditions; empty email/pass and existing email also created getUserByEmail function

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ app.set("view engine", "ejs");
 //express middleware that translates/parses incoming (req.body) of a POST/PUT request
 app.use(express.urlencoded({ extended: true }));
 
-//express middleware reads values from the cookie. 
+//express middleware reads values from the cookie.
 app.use(cookieParser());
 
 const urlDatabase = {
@@ -18,25 +18,42 @@ const urlDatabase = {
 };
 
 const users = {
-    userRandomID: {
-      id: "userRandomID",
-      email: "user@example.com",
-      password: "purple-monkey-dinosaur",
-    },
-    user2RandomID: {
-      id: "user2RandomID",
-      email: "user2@example.com",
-      password: "dishwasher-funk",
-    },
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+  user3RandomID: {
+    id: "user3RandomID",
+    email: "a@a.com",
+    password: "aa",
+  },
 };
 
 function generateRandomString() {
   let result = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
+}
+
+function getUserByEmail(email) {
+  let user;
+  const userValues = Object.values(users);
+  for (let i = 0; i < userValues.length; i++) {
+    if (Object.values(userValues[i]).indexOf(email) > -1) {
+      user = userValues[i];
+    }
+  }
+  return user ? user : null;
 }
 
 // app.get("/", (req, res) => {
@@ -54,13 +71,13 @@ function generateRandomString() {
 
 //send urlDatabase data through templateVars to urls_index.ejs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: req.cookies["user_id"]};
+  const templateVars = { urls: urlDatabase, user: req.cookies["user_id"] };
   res.render("urls_index", templateVars);
 });
 
 //render the urls_new.ejs template to present the form to the user
 app.get("/urls/new", (req, res) => {
-    const templateVars = { user: req.cookies["user_id"]};
+  const templateVars = { user: req.cookies["user_id"] };
   res.render("urls_new", templateVars);
 });
 
@@ -80,61 +97,76 @@ app.post("/urls", (req, res) => {
 //display a single URL and its shortened form
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id;
-  const templateVars = { id: shortURL, longURL: urlDatabase[shortURL], user: req.cookies["user_id"]};
+  const templateVars = {
+    id: shortURL,
+    longURL: urlDatabase[shortURL],
+    user: req.cookies["user_id"],
+  };
   res.render("urls_show", templateVars);
 });
 
 //redirects to its associated longURL directly
 app.get("/u/:id", (req, res) => {
-    const shortURL = urlDatabase[req.params.id];
-    const longURL = shortURL;
-    res.redirect(longURL);
+  const shortURL = urlDatabase[req.params.id];
+  const longURL = shortURL;
+  res.redirect(longURL);
 });
 
 //using a POST to delete a URL resource from the urlDB
 app.post("/urls/:id/delete", (req, res) => {
-    const shortURL = req.params.id;
-    delete urlDatabase[shortURL];
-    res.redirect("/urls");
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
 //update your longURL in the urlDB then redirect to /urls
 app.post("/urls/:id", (req, res) => {
-    const shortURL = req.params.id;
-    const newLongURL = req.body.newURL;
-    urlDatabase[shortURL] = newLongURL;
-    res.redirect("/urls");
+  const shortURL = req.params.id;
+  const newLongURL = req.body.newURL;
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect("/urls");
 });
 
 //login with the username cookie
 app.post("/login", (req, res) => {
-    //set cookie(username) to the value submitted in the request body via login form
-    res.cookie("user_id", req.body.username);
-    res.redirect("/urls");
+  //set cookie(username) to the value submitted in the request body via login form
+  res.cookie("user_id", req.body.username);
+  res.redirect("/urls");
 });
 
 //clear username cookie and redirect to /urls
 app.post("/logout", (req, res) => {
-    res.clearCookie("user_id");
-    res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/urls");
 });
 
 //registration page
 app.get("/register", (req, res) => {
-    const templateVars = { user: req.cookies["user_id"]};
-    res.render("register", templateVars);
+  const templateVars = { user: req.cookies["user_id"] };
+  res.render("register", templateVars);
 });
 
-//add a new user to the usersDB
+//add a new user to the users object
 app.post("/register", (req, res) => {
-    //console.log("users before:", users);
-    const id = generateRandomString();
+
+  //if email/pass is empty, respond with 400
+  if ((req.body.email === "") | (req.body.password === "")) {
+    res.status(400).send("Bad Request: enter a valid email and password");
+  } else {
     const email = req.body.email;
     const password = req.body.password;
-    users[id] = {id, email, password};
-    //console.log("users after:", users);
-    res.cookie("user_id", users[id]);
-    res.redirect("/urls");
+
+    //if the email already exists, respond with 400
+    const emailExists = getUserByEmail(email);
+    if (emailExists) {
+      res.status(400).send("Bad Request: user account already exists!");
+    } else {
+      const id = generateRandomString();
+      users[id] = { id, email, password };
+      res.cookie("user_id", users[id]);
+      res.redirect("/urls");
+    }
+  }
 });
 
 app.listen(PORT, () => {
